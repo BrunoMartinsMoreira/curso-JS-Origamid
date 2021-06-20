9090b1b60eecf6e3608a200a8cf58889d7e64b61
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-06-20 17:20:02 -0300

    basico sobre variaveis e let, var e const

---
+++ javascrip-basico/variaveis/variaveis.js
@@ -0,0 +1,12 @@
+var nome = 'Bruno'
+// palavra reservada que ja nao é usada nas versoes modernas do javascript
+
+let idade = 28
+// substitui var na versao ES6+
+
+const altura = 1.75
+// define uma constante, um valor que nao se altera
+
+// camel case - forma de declarar uma variavel com multiplos nomes
+let myPet = 'Cat'
+
