c259dc5572a345ed9bd0fc9f0f07a7b0c2c62662
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-06-20 17:56:24 -0300

    basico de tipos de dados

---
+++ javascrip-basico/tipos-de-dados/DataTypes.js
@@ -0,0 +1,20 @@
+// tipos primitivos
+
+// STRING - Palavras, simbolos e caracteres, declarados entre ''
+let nome = 'Bruno'
+
+// NUMBER - Numeros, inteiros ou nao
+let idade = 27
+let peso = 85.8
+
+// BOOLEAN - Verdadeiro ou falso, 0 ou 1
+let possuiCarro = true
+let moraNaPraia = false // quem me dera ;(
+
+// UNDEFINED - Valor indefinido
+let gostDeFutebol;
+
+//NULL - valor nulo
+let comidaFavorita = null
+
+// SYMBOL - nao entendi essa parte
