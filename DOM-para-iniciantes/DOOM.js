c4130fcd4031e515e0df106f8c7c23daae6151d5
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-06-25 20:40:02 -0300

    basico DOOM

---
+++ DOM-para-iniciantes/DOOM.js
@@ -0,0 +1,18 @@
+/**
+ * metodos para selecionar elementos no DOOM
+ * 
+ * document.querySelector() retorna o elemento passado como argumento, seja pela tag, class ou id
+ * document.querySelectorAll() retorna todos os elementos que foram passados como argumento
+ * 
+ * document.getElementById() seleciona um elemento pelo seu id [seletor CSS]
+ *document.getElementByClassName() seleciona elementos pela sua classe [seletor CSS]
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
