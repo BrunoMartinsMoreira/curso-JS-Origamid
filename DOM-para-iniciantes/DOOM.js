886310f5d281187bad0bbf2614d97ae60af989a2
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-06-25 20:42:13 -0300

    metodos para selecionar elementos da DOOM

--- DOM-para-iniciantes/DOOM.js
+++ DOM-para-iniciantes/DOOM.js
@@ -1,18 +1,11 @@
-/**
+/*
  * metodos para selecionar elementos no DOOM
  * 
- * document.querySelector() retorna o elemento passado como argumento, seja pela tag, class ou id
+ * document.querySelector() retorna o primeiro elemento passado como argumento, seja pela tag, class ou id
  * document.querySelectorAll() retorna todos os elementos que foram passados como argumento
  * 
  * document.getElementById() seleciona um elemento pelo seu id [seletor CSS]
  *document.getElementByClassName() seleciona elementos pela sua classe [seletor CSS]
- * 
- * 
- * 
- * 
- * 
- * 
- * 
  * 
  */
 
